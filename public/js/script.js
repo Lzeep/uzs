fddcce2161b56cb9a66f590fd898588f84546814
Azza1997 <jusuev.azret@mail.ru>
2019-03-14 01:37:29 +0600

    д

--- public/js/script.js
+++ public/js/script.js
@@ -1,7 +1,36 @@
 $(document).ready(function(){
+
+   var myLatLng = {lat: -24.397, lng:80.644};
+
     map = new google.maps.Map(document.getElementById("map"), {
         center: {lat: -24.397, lng:80.644},
-        
         zoom: 8
     });
+
+    //marker
+    var marker = new google.maps.Marker({
+        position: myLatLng,
+        map: map,
+        title: 'Hello World!'
+    });
+
+    var request = {
+        location: myLatLng,
+        radius: '1500',
+        type: ['school']
+    };
+
+    service = new google.maps.places.PlacesService(map);
+    service.nearbySearch(request, callback);
+
+    function callback(results, status) {
+
+        console.log(results);
+        // if (status == google.maps.places.PlacesServiceStatus.OK) {
+        //     for (var i = 0; i < results.length; i++) {
+        //         var place = results[i];
+        //         createMarker(results[i]);
+        //     }
+        // }
+    }
 });
--- resources/views/map/master.blade.php
+++ resources/views/map/master.blade.php
@@ -17,7 +17,10 @@
     <script crossorigin="anonymous" integrity="sha256-cCueBR6CsyA4/9szpPfrX3s49M9vUU5BgtiJj06wt/s=" src="https://code.jquery.com/jquery-3.1.0.min.js">
     </script>
 
-<script async="" defer="" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCVE6T2FWRqt4QN-lh-0MAinlEVkQ4rLuY">
+{{--    Googlee maps api    --}}
+
+
+<script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCVE6T2FWRqt4QN-lh-0MAinlEVkQ4rLuY">
 </script>
 <script src="{{asset('js/script.js')}}"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js">
